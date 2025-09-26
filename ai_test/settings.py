d27ae4b78dcf853a1e0ed74054ba2e5e177e3f2d
Shweta Ladne <shweta.ladne.averybit@gmail.com>
2025-09-26 12:29:35 +0530

    update setting file

--- ai_test/settings.py
+++ ai_test/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "project_name.urls"
+ROOT_URLCONF = "ai_test.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
         },
     },
 ]
-WSGI_APPLICATION = "project_name.wsgi.application"
+WSGI_APPLICATION = "ai_test.wsgi.application"
 
 
 # Database
